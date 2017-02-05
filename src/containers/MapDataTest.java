90394c3e865453c385aedc622a9292669532acec
cunkin1985 <anonymous.vn1985@gmail.com>
2017-02-05 22:21:01 +0700

    new commit

--- src/containers/MapDataTest.java
+++ src/containers/MapDataTest.java
@@ -9,7 +9,7 @@ class Letters implements Generator<Pair<Integer, String>>, Iterable<Integer>{
     private int size = 9;
     private int number = 1;
     private char letter = 'A';
-    public Pair<Integer, String> next(){
+    public Pair<Integer, String> next() {
         return new Pair<Integer, String>(number++, " " + letter++);
     }
     public Iterator<Integer> iterator(){
@@ -28,6 +28,15 @@ class Letters implements Generator<Pair<Integer, String>>, Iterable<Integer>{
 }
 public class MapDataTest {
     public static void main(String[] args){
-
+        // A Pair Generator
+        System.out.println(MapData.map(new Letters(),10));
+        //Two separate generator
+        System.out.println(MapData.map(new CountingGenerator.Character(), new RandomGenerator.String(5),10));
+        // A key generator and a single value
+        System.out.println(MapData.map(new CountingGenerator.Character(), "Value",6));
+        // A Iterable and a value generator
+        System.out.println(MapData.map(new Letters(), new RandomGenerator.String(3)));
+        // A Iterable and single value
+        System.out.println(MapData.map(new Letters(), "value"));
     }
 }
