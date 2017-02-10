a58d6ed122ce649c598058ae9fc3ae6654cda3ef
cunkin1985 <anonymous.vn1985@gmail.com>
2017-02-10 09:26:21 +0700

    new commit

--- src/example/Example_6.java
+++ src/example/Example_6.java
@@ -6,6 +6,7 @@ import java.util.*;
  */
 public class Example_6 {
     public static void test(String msg, List<String> list){
+        System.out.println("--------" + msg + "--------");
         Collection<String> collection = list;
         Collection<String> subList = list.subList(1,8);
         Collection<String> collection2 = new ArrayList<>(subList);
@@ -52,9 +53,24 @@ public class Example_6 {
         }
         try{
             list.remove(4);
-        }catch(Ex)
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            list.removeAll(collection2);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        try{
+            list.retainAll(collection2);
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
     public static void main(String[] args){
-
+        List<String> list = Arrays.asList("A B C D E F G H I J K L M N".split(" "));
+        test("Modifiable", new ArrayList<>(list));
+        test("Array.asList()", list);
+        test("Unmodifiable", Collections.unmodifiableList(list));
     }
 }
