350b7d21f0f3babb67767ddad655151a06c3f57b
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-02-11 15:42:35 +0700

    new commit

---
+++ src/example/Example_7.java
@@ -0,0 +1,16 @@
+package example;
+import java.util.*;
+import net.mindview.util.*;
+/**
+ * Created by anonymous.vn1985 on 2/11/2017.
+ */
+public class Example_7 {
+    private static ArrayList<String> arrayList = new ArrayList<>();
+    private static LinkedList<String> linkedList = new LinkedList<>();
+    public static ArrayList<String> createArrayList(){
+
+    }
+    public static void main(String[] args){
+
+    }
+}
