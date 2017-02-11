b6bad1598e87e118c0441379edf5c28db0ccc81c
cunkin1985 <anonymous.vn1985@gmail.com>
2017-02-11 23:28:05 +0700

    new commit

--- src/example/Example_7.java
+++ src/example/Example_7.java
@@ -8,7 +8,25 @@ public class Example_7 {
     private static ArrayList<String> arrayList = new ArrayList<>();
     private static LinkedList<String> linkedList = new LinkedList<>();
     public static ArrayList<String> createArrayList(){
-
+        ArrayList<String> arrayList = new ArrayList<>(Countries.names(25));
+        return arrayList;
+    }
+    public static LinkedList<String> createLinkedList(){
+        LinkedList<String> linkedList = new LinkedList<>(Countries.names(25));
+        return linkedList;
+    }
+    public static void printList(List<String> list){
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+    public static void insertByListIterator(ArrayList<String> arrayList, LinkedList<String> linkedList){
+        ListIterator<String> listIterator = linkedList.listIterator();
+        while(listIterator.hasNext()){
+            arrayList.add(listIterator.next());
+        }
     }
     public static void main(String[] args){
 
