eecfe90145068b62c471ce7e6eeff444280a31c0
cunkin1985 <anonymous.vn1985@gmail.com>
2017-02-12 22:06:38 +0700

    new commit

--- src/example/Example_8.java
+++ src/example/Example_8.java
@@ -1,9 +1,6 @@
 package example;
 import java.util.*;
 
-/**
- * Created by anonymous.vn1985 on 2/12/2017.
- */
 class Node<T>{
     private T valueNode;
     Node<T> nextNode;
@@ -12,17 +9,16 @@ class Node<T>{
         this.nextNode = nextNode;
     }
     public Node(T valueNode){
-        this(valueNode, null);
+        this(valueNode,null);
     }
     public String toString(){
         if(this.valueNode == null){
             return null;
         }else{
-            return this.valueNode.toString();
+            return valueNode.toString();
         }
     }
 }
-//Note: ListIterator and Iterator are same the cursor
 class SListIterator<T>{
     private Node<T> currentNode;
     public SListIterator(Node<T> currentNode){
@@ -35,12 +31,12 @@ class SListIterator<T>{
         currentNode = currentNode.nextNode;
         return currentNode;
     }
-    public void insert(T t){
-        currentNode.nextNode = new Node<T>(t, currentNode);
+    public void insert(T valueNewNode){
+        currentNode.nextNode = new Node<T>(valueNewNode, currentNode.nextNode);
         currentNode = currentNode.nextNode;
     }
     public void remove(){
-        if(currentNode.nextNode != null){
+        if(currentNode.nextNode != null) {
             currentNode.nextNode = currentNode.nextNode.nextNode;
         }
     }
@@ -48,28 +44,39 @@ class SListIterator<T>{
 class SList<T>{
     private Node<T> node = new Node<T>(null);
     public SListIterator<T> iterator(){
-        return new SListIterator<T>(node);
+        return new SListIterator<T>(this.node);
     }
     public String toString(){
         if(node.nextNode == null){
             return "SList: []";
         }else{
-            System.out.print("SList: [");
-            SListIterator<T> it = this.iterator();
-            StringBuilder sb = new StringBuilder();
-            while(it.hasNext()){
-                sb.append(it.next() + (it.hasNext() ? ", " : " "));
+            SListIterator<T> sl = this.iterator();
+            StringBuilder sb = new StringBuilder("[");
+            while(sl.hasNext()){
+                sb.append(sl.next() + (sl.hasNext() ? ", " : " "));
             }
             return sb + "]";
         }
     }
 }
-public class Example_8 {
+public class Example_8{
     public static void main(String[] args){
-        SList<String> sl = new SList<String>();
-        SListIterator<String> slIterator = sl.iterator();
-        System.out.println("Inserting ....");
-        slIterator.insert("Hi");
+        SList<String> sl = new SList<>();
+        SListIterator<String> iterator = sl.iterator();
+        System.out.println("sl: "+ sl);
+        iterator.insert("Hi");
+        System.out.println("sl: " + sl);
+        iterator.insert("I am");
+        iterator.insert("AI");
+        iterator.insert("I known you");
+        iterator.insert("You are ck");
+        iterator.insert("I am sure....you will live to 80 years old");
+        iterator.insert("You are anonymous");
+        System.out.println("sl: " + sl);
+        iterator.remove();
+        System.out.println(sl);
+        SListIterator<String> iterator2 = sl.iterator();
+        iterator2.remove();
         System.out.println(sl);
     }
 }
