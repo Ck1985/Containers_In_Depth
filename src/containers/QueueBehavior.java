ccc4313170d6ec44ff92d842d6526988dfed7481
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-02-16 23:23:42 +0700

    new commit

--- src/containers/QueueBehavior.java
+++ src/containers/QueueBehavior.java
@@ -9,9 +9,27 @@ import net.mindview.util.*;
 public class QueueBehavior {
     private static int count = 10;
     static <T> void test(Queue<T> queue, Generator<T> generator){
-
+        for(int i = 0; i < count; i++){
+            queue.offer(generator.next());
+        }
+        while(queue.peek() != null){
+            System.out.print(queue.remove() + " ");
+        }
+        System.out.println();
+    }
+    static class Gen implements Generator<String>{
+        String[] array = "one two three four five six seven eight nine ten".split(" ");
+        int index = 0;
+        public String next(){
+            return array[index++];
+        }
     }
     public static void main(String[] args){
-
+        test(new LinkedList<String>(),new Gen());
+        test(new PriorityQueue<String>(), new Gen());
+        test(new LinkedBlockingQueue<String>(), new Gen());
+        test(new ArrayBlockingQueue<String>(count), new Gen());
+        test(new PriorityBlockingQueue<String>(),new Gen());
+        test(new ConcurrentLinkedQueue<String>(), new Gen());
     }
 }
