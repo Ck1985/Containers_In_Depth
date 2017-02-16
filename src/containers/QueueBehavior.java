bab14f1dcb57d2001c2b41ffbae2b902fe621916
cunkin1985 <anonymous.vn1985@gmail.com>
2017-02-16 18:27:44 +0700

    new commit

---
+++ src/containers/QueueBehavior.java
@@ -0,0 +1,17 @@
+package containers;
+import java.util.*;
+import java.util.concurrent.*;
+import net.mindview.util.*;
+
+/**
+ * Created by anonymous on 2/16/2017.
+ */
+public class QueueBehavior {
+    private static int count = 10;
+    static <T> void test(Queue<T> queue, Generator<T> generator){
+
+    }
+    public static void main(String[] args){
+
+    }
+}
