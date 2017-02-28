b72091ef5748da0cc2350a65f361a91bd0015224
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-02-28 10:30:20 +0700

    new commit

--- src/genericsAndCollections/DVDApplication.java
+++ src/genericsAndCollections/DVDApplication.java
@@ -1,7 +1,37 @@
 package genericsAndCollections;
+import java.util.*;
+import net.mindview.util.*;
 
 /**
  * Created by anonymous.vn1985 on 2/27/2017.
  */
 public class DVDApplication {
+    private static ArrayList<DVDInfo> dvdList = new ArrayList<>();
+    private static DVDInfo dvdInfo = null;
+    static void populateData(){
+        String title = null;
+        String genre = null;
+        String leadActor = null;
+        String extraData = TextFile.read("C:\\Users\\Dell Vostro 5459\\IdeaProjects\\Containers_In_Depth\\src\\genericsAndCollections\\dvdinfo.txt");
+        String[] arrayData = extraData.split("\n");
+        String[] arrayKind;
+        for (String string : arrayData){
+            arrayKind = string.split("/");
+            for (int i = 0; i < arrayKind.length; i++) {
+                 if (i == 0){
+                     title = arrayKind[i];
+                 }else if (i == 1){
+                     genre = arrayKind[i];
+                 }else {
+                     leadActor = arrayKind[i];
+                 }
+            }
+            dvdInfo = new DVDInfo(title,genre,leadActor);
+            dvdList.add(dvdInfo);
+        }
+        System.out.println(dvdList);
+    }
+    public static void main(String[] args){
+       populateData();
+    }
 }
--- src/genericsAndCollections/DVDInfo.java
+++ src/genericsAndCollections/DVDInfo.java
@@ -1,7 +1,18 @@
 package genericsAndCollections;
-import net.mindview.util.TextFile;
+
 /**
  * Created by anonymous.vn1985 on 2/27/2017.
  */
 class DVDInfo {
+    String title;
+    String genre;
+    String leadActor;
+    public DVDInfo(String title, String genre, String leadActor){
+        this.title = title;
+        this.genre = genre;
+        this.leadActor = leadActor;
+    }
+    public String toString(){
+        return "Title: " + this.title + "-----Genre: " + this.genre + "----LeadActor: " + this.leadActor + " \n";
+    }
 }
