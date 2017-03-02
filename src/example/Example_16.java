d314b398b0e7f54ea100e9812ef67af996895626
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-03-02 23:16:52 +0700

    new commit

--- src/example/Example_16.java
+++ src/example/Example_16.java
@@ -5,7 +5,9 @@ import java.util.*;
  * Created by anonymous on 3/2/2017.
  */
 class SlowMap<K, V> extends AbstractMap<K, V>{
-
+    public V put(K key, V value){
+        return this.put(key, value);
+    }
 }
 public class Example_16 {
     public static void main(String[] args){
