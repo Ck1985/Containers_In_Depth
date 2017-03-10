bda1b0e4d009f05e87a1a775d7eeabed65b079d0
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-03-10 11:38:34 +0700

    new commit

--- src/example/Example_22.java
+++ src/example/Example_22.java
@@ -25,31 +25,100 @@ class MapEntry<K,V> implements Map.Entry<K,V>{
         return oldValue;
     }
     public int hashCode(){
-        return (this.key.hashCode()) ^ (this.value.hashCode());
+        return (this.key == null ? 0 : this.key.hashCode()) ^ (this.value == null ? 0 : this.value.hashCode());
     }
     public boolean equals(Object object){
         if (!(object instanceof containers.MapEntry)){
             return false;
         } else {
-            return (this.getKey().equals(((MapEntry)object).getKey()))
+            return (this.key == null ? ((MapEntry)object).getKey() == null : this.getKey().equals(((MapEntry)object).getKey()))
                     &&
-                    (this.getValue().equals(((MapEntry)object).getValue()));
+                    (this.value == null ? ((MapEntry)object).getValue() == null : this.getValue().equals(((MapEntry)object).getValue()));
         }
     }
 }
 class SimpleHashMap<K,V> extends AbstractMap<K,V>{
     private static final int SIZE = 97;
     @SuppressWarnings("unchecked")
-    protected LinkedList<Map.Entry<K,V>>[] buckets = new LinkedList[SIZE];
+    protected LinkedList<MapEntry<K,V>>[] buckets = new LinkedList[SIZE];
     public V put(K key, V value){
+        V oldValue = null;
         int index = Math.abs(key.hashCode()) % SIZE;
         if (this.buckets[index] == null){
-            this.buckets[index] = new LinkedList<Map.Entry<K,V>>();
+            this.buckets[index] = new LinkedList<MapEntry<K,V>>();
         }
+        LinkedList<MapEntry<K,V>> bucket = this.buckets[index];
+        MapEntry<K,V> pair = new MapEntry<>(key, value);
+        boolean found = false;
+        ListIterator<MapEntry<K,V>> iterator = bucket.listIterator();
+        while(iterator.hasNext()){
+            MapEntry<K,V> iPair = iterator.next();
+            if (iPair.getKey().equals(key)){
+                iterator.set(pair);
+                found = true;
+                break;
+            }
+        }
+        if (!found){
+            this.buckets[index].add(pair);
+        }
+        return oldValue;
+    }
+    public V get(Object key){
+        int index = Math.abs(key.hashCode()) % SIZE;
+        if (this.buckets[index] == null){
+            return null;
+        }
+        for (MapEntry<K,V> entry : this.buckets[index]){
+            if (entry.getKey().equals(key)){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+    public Set<Map.Entry<K,V>> entrySet(){
+        Set<Map.Entry<K,V>> entrySet = new HashSet<>();
+        for (LinkedList<MapEntry<K,V>> bucket : buckets){
+            if (bucket == null){
+                continue;
+            }
+            for (MapEntry<K,V> entry : bucket){
+                entrySet.add(entry);
+            }
+        }
+        return entrySet;
+    }
+    public void clear(){
+        this.entrySet().clear();
+    }
+    public V remove(Object key){
+        V value = null;
+        if (this.get(key) != null){
+            int index = Math.abs(key.hashCode()) % SIZE;
+            for (MapEntry<K,V> entry : buckets[index]){
+                if (entry.getKey().equals(key)){
+                    value = entry.getValue();
+                    int i = buckets[index].indexOf(entry);
+                    buckets[index].remove(i);
+                    break;
+                }
+            }
+        }
+        return value;
     }
 }
 public class Example_22 {
     public static void main(String[] args){
-
+        SimpleHashMap<String,String> m =
+                new SimpleHashMap<String,String>();
+        m.putAll(containers.Countries.capitals(5));
+        System.out.println(m);
+        System.out.println(m.get("ALGERIA"));
+        System.out.println(m.remove("ALGERIA"));
+        System.out.println(m.get("ALGERIA"));
+        System.out.println(m.remove("ANGOLA"));
+        System.out.println(m);
+        m.clear();
+        System.out.println(m);
     }
 }
