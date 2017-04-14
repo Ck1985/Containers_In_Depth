2d5de21f8580bcf1bd51c495591e253278721411
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-04-15 00:19:05 +0700

    ,

---
+++ src/example/Example_40.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.*;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+
+class Employee {
+    private String name;
+    private String id;
+}
+public class Example_40 {
+    public static void main(String[] args) {
+
+    }
+}
