993e8c7bf0ed78f6a217d1b0b3459dedc4071dd0
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-04-18 00:54:11 +0700

    New Commit

---
+++ src/containers/Enumerations.java
@@ -0,0 +1,19 @@
+package containers;
+
+import java.util.*;
+import net.mindview.util.*;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class Enumerations {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>(Countries.names(10));
+        Enumeration<String> enumeration = vector.elements();
+        while (enumeration.hasMoreElements()) {
+            System.out.print(enumeration.nextElement() + ", ");
+        }
+        //Procedure an Enumeration from Collection
+        enumeration = Collections.enumeration(new ArrayList<String>());
+    }
+}
